840b6b137780071257a019dc7a14f0787110780b
Ravi Kumar Gupta <ravikumargupta833@gmail.com>
2026-08-08 22:21:21 +0530

    Add files via upload

---
+++ DSA-Challenge-main/0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public void moveZeroes(int[] nums) {
+
+        int write = 0;
+
+        for (int read = 0; read < nums.length; read++) {
+
+            if (nums[read] != 0) {
+
+                nums[write] = nums[read];
+
+                write++;
+            }
+        }
+
+        while (write < nums.length) {
+
+            nums[write] = 0;
+
+            write++;
+        }
+    }
+}
