429dfd540f2812805ace87d4f0298ecb555ed7d3
Ravi Kumar Gupta <ravikumargupta833@gmail.com>
2026-09-01 22:11:34 +0530

    Add 749A Bachgold Problem solution

---
+++ 02-Mathematics/749A_Bachgold_Problem.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int count = n / 2;
+        System.out.println(count);
+
+        for (int i = 0; i < count; i++) {
+            if (n % 2 == 1 && i == count - 1) {
+                System.out.print(3);
+            } else {
+                System.out.print(2);
+            }
+
+            if (i < count - 1) {
+                System.out.print(" ");
+            }
+        }
+
+        System.out.println();
+    }
+}
